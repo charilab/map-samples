662878b64e3207a6af97e24e8a6d70f17fea68ec
User Charilab <charilab@www2541.sakura.ne.jp>
2017-01-08 14:24:49 +0900

    Replace libPOI.js to the latest one

--- libs/libPOI.js
+++ libs/libPOI.js
@@ -1,94 +1,105 @@
+const IMAGE_PATH="/images";
+
 var convIcons = {
     "セブン-イレブン": L.icon({
-        iconUrl: '/images/conv_711_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_711_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "セブンイレブン": L.icon({
-        iconUrl: '/images/conv_711_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_711_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "ローソン": L.icon({
-        iconUrl: '/images/conv_lawson_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_lawson_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "LAWSON": L.icon({
-        iconUrl: '/images/conv_lawson_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_lawson_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "Lawson": L.icon({
-        iconUrl: '/images/conv_lawson_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_lawson_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "ファミリーマート": L.icon({
-        iconUrl: '/images/conv_famima_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_famima_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "Family": L.icon({
-        iconUrl: '/images/conv_famima_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_famima_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "FamilyMart": L.icon({
-        iconUrl: '/images/conv_famima_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_famima_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "Familymart": L.icon({
-        iconUrl: '/images/conv_famima_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_famima_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "ミニストップ": L.icon({
-        iconUrl: '/images/conv_ministop_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_ministop_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "サークルK": L.icon({
-        iconUrl: '/images/conv_circlek_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_circlek_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "デイリーヤマザキ": L.icon({
-        iconUrl: '/images/conv_daily_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_daily_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "ポプラ": L.icon({
-        iconUrl: '/images/conv_poplar_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_poplar_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "スリーエフ": L.icon({
-        iconUrl: '/images/conv_threef_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_threef_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "サンクス": L.icon({
-        iconUrl: '/images/conv_sunkus_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_sunkus_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "ココストア": L.icon({
-        iconUrl: '/images/conv_coco_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_coco_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "セイコーマート": L.icon({
-        iconUrl: '/images/conv_seico_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_seico_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
     "others": L.icon({
-        iconUrl: '/images/conv_others_32.png',
-        iconSize: [32, 32], 
-        iconAnchor: [16, 16]}),
+        iconUrl: IMAGE_PATH+'/conv_others_20.png',
+        iconSize: [20, 20], 
+        iconAnchor: [10, 10]}),
 };
 
 var passIcon = L.icon({
-    iconUrl: '/images/flag.png',
-    iconSize:     [48, 48],
-    iconAnchor:   [24, 48],
-    popupAnchor:  [-3, -76]
+    iconUrl: IMAGE_PATH+'/pass_16.png',
+    iconSize:     [16, 16],
+    iconAnchor:   [8, 8]
 });
 
 var spaIcon = L.icon({
-    iconUrl: '/images/hotspring_32.png',
+    iconUrl: IMAGE_PATH+'/hotspring_32.png',
     iconSize: [32, 32], 
     iconAnchor: [16, 16]});
 
 var bicycleIcon = L.icon({
-    iconUrl: '/images/bicycle_32.png',
+    iconUrl: IMAGE_PATH+'/bicycle_32.png',
     iconSize: [32, 32], 
     iconAnchor: [16, 16]});
+
+var toiletsIcon = L.icon({
+    iconUrl: IMAGE_PATH+'/toilets_16.png',
+    iconSize: [16, 16], 
+    iconAnchor: [8, 8]});
+
+var servicesIcon = L.icon({
+    iconUrl: IMAGE_PATH+'/michinoeki.svg',
+    iconSize: [16, 16], 
+    iconAnchor: [8, 8]});
 
 function selectIcon(feature) {
     var icon = convIcons["others"];
@@ -115,6 +126,10 @@ function createMarker(feature, latlng) {
     } else if ((feature.properties.shop == "bicycle")
         || (feature.properties.shop == "sports")) {
         var marker = new L.Marker(latlng, {icon: bicycleIcon});
+    } else if (feature.properties.highway == "services") {
+        var marker = new L.Marker(latlng, {icon: servicesIcon});
+    } else if (feature.properties.amenity == "toilets") {
+        var marker = new L.Marker(latlng, {icon: toiletsIcon});
     } else {
         var marker = new L.CircleMarker(latlng);
     }
